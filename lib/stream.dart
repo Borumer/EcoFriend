b80a52d07ac3f6455708a1134c5b96a4d3d17b9a
disnerd28 <60379504+disnerd28@users.noreply.github.com>
2020-04-26 09:56:24 -0500

    http request test

--- lib/stream.dart
+++ lib/stream.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 import 'dart:math';
+import 'nav.dart';
+
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Stream extends StatefulWidget {
   Stream({Key key, this.title}) : super(key: key);
@@ -14,14 +18,55 @@ class Stream extends StatefulWidget {
 
 class _StreamState extends State<Stream> {
 
+  Map data;
+  List userData;
+
+  Future getData() async {
+    http.Response response = await http.get("https://reqres.in/api/users?page=2");
+    data = json.decode(response.body);
+    setState(() {
+      userData = data["data"];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Feed',
+      title: 'Stream',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new InfiniteListView(),
+      home: ListView.builder(
+        itemCount: userData == null ? 0 : userData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(userData[index]["avatar"]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("${userData[index]["first_name"]} ${userData[index]["last_name"]}",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                      ),),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
 
 
@@ -113,7 +158,7 @@ class InfiniteListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text('Feed')),
+      appBar: new AppBar(title: new Text('Stream')),
       body: new ListView.builder(
           itemCount: total,
           itemBuilder: (BuildContext context, int index) => _loadItem(index)),
