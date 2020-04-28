21446755e79e7e83a7e2184e4e69cc53c1810a4a
Varun <31084854+Borumer@users.noreply.github.com>
2020-04-28 18:34:37 -0400

    Fix assets in pubspec

--- lib/register.dart
+++ lib/register.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 
 class Register extends StatefulWidget {
